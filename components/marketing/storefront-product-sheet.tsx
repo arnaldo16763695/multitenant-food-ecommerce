52033115bed3585954b7c76568f872139e3efe60
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2026-09-19 18:18:53 -0400

    Revert "fix(storefront): fix undefined variable reference breaking the deploy build"
    This reverts commit 74070d39bb1e90b7b85024f80f4e7739f81ae717.

--- components/marketing/storefront-product-sheet.tsx
+++ components/marketing/storefront-product-sheet.tsx
@@ -244,7 +244,7 @@ export function StorefrontProductSheet({ tenantSlug, branchId, product, open, on
           productId: product.id,
           productVariantId: selectedVariant?.id ?? null,
           baseQuantity: baseSplitQuantity,
-          baseModifierSelections: baselineModifierSelections,
+          baseModifierSelections,
           customQuantity,
           customModifierSelections: modifierSelections,
         })
@@ -285,7 +285,7 @@ export function StorefrontProductSheet({ tenantSlug, branchId, product, open, on
         productId: product.id,
         productVariantId: selectedVariant?.id ?? null,
         baseQuantity: baseSplitQuantity,
-        baseModifierSelections: baselineModifierSelections,
+        baseModifierSelections,
         customQuantity,
         customModifierSelections: modifierSelections,
       })
